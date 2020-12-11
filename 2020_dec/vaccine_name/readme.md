b40b461d437d72a0ea7760dbd4217fc13f0e1ede
Marc Neumann <marc.neumann@bmw.de>
2020-12-11 23:17:48 +0100

    structured and beautified vaccine code to continue with next challenge

---
+++ 2020_dec/vaccine_name/readme.md
@@ -0,0 +1,3 @@
+# Vaccine Problem
+
+The file [vaccine.py](vaccine.py) solves the problem https://www.codechef.com/DEC20B/problems/VACCINE1.
---
+++ 2020_dec/vaccine_name/test_vaccine.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+import subprocess
+import os
+
+
+class Test(TestCase):
+    def test_1(self):
+        result = self.run_problem_solver(1, 2, 1, 3, 14)
+        self.assertEqual(result, b'3\n')
+
+    def test_2(self):
+        result = self.run_problem_solver(5, 4, 2, 10, 100)
+        self.assertEqual(result, b'9\n')
+
+    def run_problem_solver(self, d1, v1, d2, v2, p):
+        proc = subprocess.Popen(
+            ['python', 'vaccine.py', str(d1), str(v1), str(d2), str(v2), str(p)],
+            cwd=os.path.dirname(os.path.realpath(__file__)),
+            stdout=subprocess.PIPE,
+            stdin=subprocess.PIPE)
+        input_line = str(d1) + ' ' + str(v1) + ' ' + str(d2) + ' ' + str(v2) + ' ' + str(p) + '\n'
+        proc.stdin.write(bytes(input_line, 'utf-8'))
+        proc.stdin.close()
+        proc.wait()
+        output = proc.stdout.readline()
+        proc.stdout.close()
+        return output
