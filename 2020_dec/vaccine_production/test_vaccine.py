0b07d93ee737f344794e68244c6fef1519d618d5
Marc Neumann <marc.neumann@bmw.de>
2020-12-12 16:06:20 +0100

    removed obsolete vaccine program args since stdin is used

--- 2020_dec/vaccine_production/test_vaccine.py
+++ 2020_dec/vaccine_production/test_vaccine.py
@@ -14,7 +14,7 @@ class Test(TestCase):
 
     def run_problem_solver(self, d1, v1, d2, v2, p):
         proc = subprocess.Popen(
-            ['python', 'vaccine.py', str(d1), str(v1), str(d2), str(v2), str(p)],
+            ['python', 'vaccine.py'],
             cwd=os.path.dirname(os.path.realpath(__file__)),
             stdout=subprocess.PIPE,
             stdin=subprocess.PIPE)
