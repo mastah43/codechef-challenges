dad44504f34a8c5ec09ca7c4eecc80e71236aa32
Marc Neumann <marc.neumann@bmw.de>
2020-12-25 20:16:29 +0100

    completed vaccine distribution problem

--- tests/test_vaccine_distribution.py
+++ tests/test_vaccine_distribution.py
@@ -2,23 +2,26 @@ from unittest import TestCase
 import subprocess
 from typing import List
 from vaccine_distribution.vaccine_distribution import Case
+from vaccine_distribution.vaccine_distribution import days_to_vaccine_distributed
 
 
 class Test(TestCase):
     def test_1(self):
-        result = self.run_problem_solver(1, [1])
-        self.assertEqual([1], result)
+        self.assertEqual(1, days_to_vaccine_distributed(1, [1]))
 
     def test_2(self):
+        self.assertEqual(2, days_to_vaccine_distributed(2, [6, 99, 25, 30]))
+
+    def test_main_2(self):
         result = self.run_problem_solver(2, [6, 99, 25, 30])
         self.assertEqual([2], result)
 
-    def test_example_1(self):
+    def test_main_example_1(self):
         result = self.run_problem_solver(1, [10, 20, 30, 40, 50, 60, 90, 80, 100, 1])
         self.assertEqual([10], result)
 
-    def test_example_2(self):
-        result = self.run_problem_solver(1, [9, 80, 27, 72, 79])
+    def test_main_example_2(self):
+        result = self.run_problem_solver(2, [9, 80, 27, 72, 79])
         self.assertEqual([3], result)
 
     def run_problem_solver(self, d: int, ages: List[int]) -> List[int]:
@@ -45,7 +48,7 @@ class Test(TestCase):
             self.write_input_case(proc, case)
 
     def write_input_case(self, proc, case: Case):
-        self.write_stdin_line(proc, str(case.d) + ' ' + str(len(case.ages)) + '\n')
+        self.write_stdin_line(proc, str(len(case.ages)) + ' ' + str(case.d) + '\n')
         self.write_stdin_line(proc, ' '.join([str(age) for age in case.ages]) + '\n')
 
     def write_stdin_line(self, proc, line: str):
--- vaccine_distribution/vaccine_distribution.py
+++ vaccine_distribution/vaccine_distribution.py
@@ -1,4 +1,6 @@
 import sys
+import math
+from functools import reduce
 from typing import List
 
 
@@ -13,12 +15,18 @@ def print_err(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 
-def is_risk_age(age) -> bool:
+def is_risk_age(age: int) -> bool:
     return age <= 9 or age >= 80
 
 
+def is_no_risk_age(age: int) -> bool:
+    return not is_risk_age(age)
+
+
 def days_to_vaccine_distributed(people_per_day: int, ages: List[int]) -> int:
-    return 0
+    count_people_risk = sum(1 for age in filter(is_risk_age, ages))
+    count_people_no_risk = len(ages) - count_people_risk
+    return math.ceil(count_people_risk / people_per_day) + math.ceil(count_people_no_risk / people_per_day)
 
 
 def read_input() -> List[Case]:
